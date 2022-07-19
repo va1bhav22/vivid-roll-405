ddfac26ef42f4680679f6680cbe38d863c45c58f
Kundan Kumar <kd181197@gmail.com>
2022-07-19 16:33:52 +0530

    innitial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import logo from './logo.svg';
 import './App.css';
+import Login from './Pages/Login';
+import MainRoutes from './Pages/MainRoutes';
+import Signup from './Pages/Signup';
 
 function App() {
   return (
     <div className="App">
-     
+    
     </div>
   );
 }
