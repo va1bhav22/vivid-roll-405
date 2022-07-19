cbe5d281bf17616128a432b6f1355aba8ac0a7d2
Kundn1811 <96390652+Kundn1811@users.noreply.github.com>
2022-07-19 16:37:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import Login from './Pages/Login';
-import MainRoutes from './Pages/MainRoutes';
-import Signup from './Pages/Signup';
+
 
 function App() {
   return (
